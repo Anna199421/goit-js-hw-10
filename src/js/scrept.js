9fe0ad80e62749d45060c713394f315703ace99d
Anna199421 <111239829+Anna199421@users.noreply.github.com>
2023-02-12 11:37:53 +0200

    ffff

---
+++ src/js/scrept.js
@@ -0,0 +1,11 @@
+const BASE_URL = 'https://restcountries.com/v2/name/';
+const searchParams = '?fields=name,capital,population,flags,languages';
+
+export function fetchCountries(name) {
+  return fetch(`${BASE_URL}${name}${searchParams}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
